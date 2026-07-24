edf203baf26c276dd82c1b5db48748abd5bd5a27
Gabriel <maguinso@gmail.com>
2026-07-23 22:46:20 -0300

    agregar control de entradas de google

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from flask import Flask, render_template, redirect, url_for
+import sqlite3
+
+conexion = sqlite3.connect("usuarios.db")
+cursor = conexion.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS  usuarios(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre_usu TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL,
+        contraseña TEXT NOT NULL
+    )
+""")
+conexion.commit()
+conexion.close
+
 
 app = Flask(__name__)
 
@@ -9,6 +24,8 @@ def index():
 @app.route("/endesarrollo")
 def endesarrollo():
     return render_template("endesarrollo.html")
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
